66fb882209462f75930e2cdb50747a39d1362184
Juan Torres <torrescjuan@javeriana.edu.co>
2024-07-01 12:13:11 -0500

    model task crated and migartions done

--- base/models.py
+++ base/models.py
@@ -1,3 +1,28 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    done = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self) -> str:
+        return self.title
+    
+    class Meta:
+        ordering = ['done', '-created']
+        verbose_name = 'Task'
+        verbose_name_plural = 'Tasks'
+        constraints = [
+            models.UniqueConstraint(fields=['user', 'title'], name='unique_task_per_user')
+        ]
+    
+    def mark_as_done(self):
+        self.done = True
+        self.save()
+
+    def mark_as_pending(self):
+        self.done = False
+        self.save()

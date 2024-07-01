8b59b82f8a34beba59378b6578393f49dcfdc8ff
Juan Torres <torrescjuan@javeriana.edu.co>
2024-07-01 13:36:06 -0500

    detail and create class views done without template

--- base/urls.py
+++ base/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>/', views.TaskDetail.as_view(), name='task'),
+    path('task-create/', views.TaskCreate.as_view(), name='task-create'),
 ]

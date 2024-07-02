fa423285c007fa6e17fe714b54e4a58a9ab3b97e
Juan Torres <torrescjuan@javeriana.edu.co>
2024-07-02 12:32:05 -0500

    LoginRequiredMixin done

--- base/urls.py
+++ base/urls.py
@@ -3,8 +3,8 @@ from django.contrib.auth.views import LogoutView
 from . import views
 
 urlpatterns = [
-    path('login/', views.CustomLoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('account/login/', views.CustomLoginView.as_view(), name='login'),
+    path('account/logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('', views.TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', views.TaskDetail.as_view(), name='task'),
     path('task-create/', views.TaskCreate.as_view(), name='task-create'),

30a66c620567730964a30f2bd088c8b281aca190
Juan Torres <torrescjuan@javeriana.edu.co>
2024-07-03 10:58:25 -0500

    Refactor code for PEP 8 compliance and improved readability in views and urls. Added register view with corresponding template.

--- base/urls.py
+++ base/urls.py
@@ -2,9 +2,14 @@ from django.urls import path
 from django.contrib.auth.views import LogoutView
 from . import views
 
+# URL patterns for the application
 urlpatterns = [
+    # Account-related URLs
     path('account/login/', views.CustomLoginView.as_view(), name='login'),
     path('account/logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('account/register/', views.RegisterUser.as_view(), name='register'),
+
+    # Task-related URLs
     path('', views.TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', views.TaskDetail.as_view(), name='task'),
     path('task-create/', views.TaskCreate.as_view(), name='task-create'),
--- base/views.py
+++ base/views.py
@@ -1,15 +1,21 @@
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 from .models import Task
+from django.shortcuts import redirect
 
 
 class CustomLoginView(LoginView):
+    """
+    View to handle user login.
+    """
     template_name = 'base/login.html'
     fields = '__all__'
     redirect_authenticated_user = True
@@ -17,36 +23,90 @@ class CustomLoginView(LoginView):
     def get_success_url(self):
         return reverse_lazy('tasks')
 
-# Create your views here.
+
+class RegisterUser(FormView):
+    """
+    View to handle user registration.
+    """
+    template_name = 'base/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('tasks')
+    
+    def form_valid(self, form):
+        """
+        If the form is valid, save the new user and log them in.
+        """
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
+    
+    def get(self, *args, **kwargs):
+        """
+        If user is authenticated, redirect to tasks.
+        """
+        if self.request.user.is_authenticated:
+            return redirect('tasks')
+        return super().get(*args, **kwargs)
+    
+
 class TaskList(LoginRequiredMixin, ListView):
+    """
+    View to display a list of tasks for the logged-in user.
+    """
     model = Task
     context_object_name = 'tasks'
     
     def get_context_data(self, **kwargs):
+        """
+        Add additional context including count of incomplete tasks.
+        """
         context = super().get_context_data(**kwargs)
-        context['tasks'] = context['tasks'].filter(user=self.request.user)
-        context['count'] = context['tasks'].filter(done=False).count()
+        user_tasks = context['tasks'].filter(user=self.request.user)
+        context['tasks'] = user_tasks
+        context['count'] = user_tasks.filter(done=False).count()
         return context
-    
+
+
 class TaskDetail(LoginRequiredMixin, DetailView):
+    """
+    View to display details of a specific task.
+    """
     model = Task
     context_object_name = 'task'
     template_name = 'base/task.html'
-    
+
+
 class TaskCreate(LoginRequiredMixin, CreateView):
+    """
+    View to handle creation of a new task.
+    """
     model = Task
     fields = ['title', 'description', 'done']
     success_url = reverse_lazy('tasks')
     
+    def form_valid(self, form):
+        """
+        Set the user of the task to the logged-in user.
+        """
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
 class TaskUpdate(LoginRequiredMixin, UpdateView):
+    """
+    View to handle updating an existing task.
+    """
     model = Task
     fields = ['title', 'description', 'done']
     success_url = reverse_lazy('tasks')
 
+
 class TaskDelete(LoginRequiredMixin, DeleteView):
+    """
+    View to handle deletion of a task.
+    """
     model = Task
     context_object_name = 'task'
     success_url = reverse_lazy('tasks')
-    
-    
-    

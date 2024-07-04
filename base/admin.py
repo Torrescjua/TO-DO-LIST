d24fa97ef9461cb9bb97da6f6ca54d5135f0ef25
Juan Torres <torrescjuan@javeriana.edu.co>
2024-07-04 09:39:00 -0500

    Refactor code for PEP 8 compliance and improved readability. Enhanced Task model and admin configuration.

--- base/admin.py
+++ base/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Task
 
+class TaskAdmin(admin.ModelAdmin):
+    """
+    Custom admin class for the Task model.
+    """
+    list_display = ('title', 'user', 'done', 'created')
+    list_filter = ('done', 'created')
+    search_fields = ('title', 'description', 'user__username')
+    ordering = ('-created', 'done')
+
 # Register your models here.
-admin.site.register(Task)
+admin.site.register(Task, TaskAdmin)
--- base/apps.py
+++ base/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 
-
 class BaseConfig(AppConfig):
+    """
+    Configuration class for the base application.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'base'

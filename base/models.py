eba533908c4b92161ec8d45515c5c20828f0c962
Juan Torres <torrescjuan@javeriana.edu.co>
2024-07-04 09:39:00 -0500

    Refactor code for PEP 8 compliance and improved readability. Enhanced Task model and admin configuration.

--- base/models.py
+++ base/models.py
@@ -1,16 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
+
 # Create your models here.
 class Task(models.Model):
+    """
+    A model representing a task with a title, description, status, and associated user.
+    """
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     done = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
-    
+
     def __str__(self) -> str:
         return self.title
-    
+
     class Meta:
         ordering = ['done', '-created']
         verbose_name = 'Task'
@@ -18,11 +22,17 @@ class Task(models.Model):
         constraints = [
             models.UniqueConstraint(fields=['user', 'title'], name='unique_task_per_user')
         ]
-    
+
     def mark_as_done(self):
+        """
+        Mark the task as done.
+        """
         self.done = True
         self.save()
 
     def mark_as_pending(self):
+        """
+        Mark the task as pending.
+        """
         self.done = False
         self.save()
